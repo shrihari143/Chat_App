e84016d132731ba74a97474ddad227206d7c61bb
Shrihari Chaurasia <126378618+shrihari143@users.noreply.github.com>
2023-08-13 17:37:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const path = require('path');
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public/build'));
 app.use("/api/auth",userRoutes );
 app.use("/api/msg",msgRoutes );
 
-app.use(express.static('public/build'));
 // app.use(express.static(path.join(__dirname,'/public/build')));
 // app.get("*",function(req,res){
 //   res.sendFile(path.join(__dirname,"./public/build/index.html"));
